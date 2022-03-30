d295289ec264efb9ace7a27f4ceaa57e88d664e2
Ibrahimkhalil9 <antor7938@gmail.com>
2022-03-30 19:52:32 +0600

    fist

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <div className='bg-indigo-500 p-2'>
-            <Link className='text-2xl mr-2 bg-red-400 rounded px-4' to='/'>Home</Link>
-            <Link className='text-2xl mr-2 bg-blue-300 rounded px-4' to='/foods'>Foods</Link>
 
+const Header = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className='bg-indigo-500 py-4'>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <div className={`md:flex justify-center  w-full md:static absolute duration-500 ease-in ${open ? "top-6" : "top-[-120px]"}`}>
+
+                <Link className='text-2xl mr-2 bg-red-400 rounded px-4' to='/'>Home</Link>
+                <Link className='text-2xl mr-2 bg-blue-300 rounded px-4' to='/foods'>Foods</Link>
+
+
+            </div>
         </div>
 
 
